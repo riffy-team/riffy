f994457f14adc245a01f4485e4ca765f05b2ab49
ryzvision <kandepatilkunal@gmail.com>
2023-09-05 20:04:05 +0530

    Modified track for plugin support

--- build/structures/Track.js
+++ build/structures/Track.js
@@ -1,4 +1,5 @@
 const { getImageUrl } = require("../functions/fetchImage");
+const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 class Track {
     constructor(data, requester, node) {
@@ -27,6 +28,50 @@ class Track {
         } else {
             this.info.thumbnail = getImageUrl(this.info)
         }
+    }
+
+    async resolve(riffy) {
+        const query = [this.info.author, this.info.title].filter((x) => !!x).join(" - ");
+        const result = await riffy.resolve({ query, source: riffy.options.defaultSearchPlatform, requester: this.info.requester });
+
+        if (!result || !result.tracks.length) {
+            return;
+        }
+
+        const officialAudio = result.tracks.find((track) => {
+            const author = [this.info.author, `${this.info.author} - Topic`];
+            return author.some((name) => new RegExp(`^${escapeRegExp(name)}$`, "i").test(track.info.author)) ||
+                new RegExp(`^${escapeRegExp(this.info.title)}$`, "i").test(track.info.title);
+        });
+
+        if (officialAudio) {
+            this.info.identifier = officialAudio.info.identifier;
+            this.track = officialAudio.track;
+            return this;
+        }
+
+        if (this.info.length) {
+            const sameDuration = result.tracks.find((track) => track.info.length >= (this.info.length ? this.info.length : 0) - 2000 &&
+                track.info.length <= (this.info.length ? this.info.length : 0) + 2000);
+
+            if (sameDuration) {
+                this.info.identifier = sameDuration.info.identifier;
+                this.track = sameDuration.track;
+                return this;
+            }
+
+            const sameDurationAndTitle = result.tracks.find((track) => track.info.title === this.info.title && track.info.length >= (this.info.length ? this.info.length : 0) - 2000 && track.info.length <= (this.info.length ? this.info.length : 0) + 2000);
+
+            if (sameDurationAndTitle) {
+                this.info.identifier = sameDurationAndTitle.info.identifier;
+                this.track = sameDurationAndTitle.track;
+                return this;
+            }
+        }
+
+        this.info.identifier = result.tracks[0].info.identifier;
+        this.track = result.tracks[0].track;
+        return this;
     }
 }
 
