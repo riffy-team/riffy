2f2003f8a8e44ca194444996f4131ee79112adb7
Emmanuel Lobo <76094069+UnschooledGamer@users.noreply.github.com>
2026-01-02 21:01:12 +0530

    Update build/structures/Queue.js

--- build/structures/Queue.js
+++ build/structures/Queue.js
@@ -3,10 +3,6 @@ class Queue extends Array {
         return this.length;
     }
 
-    get isEmpty() {
-        return this.length === 0;
-    }
-
     get first() {
         return this.length ? this[0] : null;
     }
